909a7bf2f023f691ee61278d0166a4d8dd2abc65
Billy Watson <billy.watson@thisisabsurd.com>
2020-11-02 22:33:58 +0000

    hero/image slider component, used tiny-slider & created multiple img crop sizes

---
+++ src/assets/scripts/components/slider.js
@@ -0,0 +1,31 @@
+import { tns } from './../../../../node_modules/tiny-slider/src/tiny-slider';
+
+const initImageSlider = () => {
+
+  const $slider = document.querySelector('[data-slider-image]');
+
+  if (!$slider) return false;
+
+  const slider = tns({
+    container: $slider,
+    autoplay: true,
+    lazyload: true,
+    lazyloadSelector: '[data-lazy]',
+    mouseDrag: true,
+    controlsPosition: 'bottom',
+    rewind: true,
+    navPosition: 'bottom',
+    autoplayButtonOutput: false
+  });
+
+  return slider;
+
+}
+
+const init = () => {
+
+  initImageSlider();
+
+}
+
+export default init;
--- src/assets/scripts/index.js
+++ src/assets/scripts/index.js
@@ -1,1 +1,9 @@
+import initSlider from './components/slider';
 
+const init = () => {
+
+  initSlider();
+
+}
+
+init();
